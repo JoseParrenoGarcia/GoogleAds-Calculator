9c6e89436fae581ba6ccfdea4c7e06de50f12199
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-07-17 11:21:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,3 +55,91 @@ with st.container(border=True):
 
             else:
                 st.warning('Please provide a value for revenue per click and target ROAS')
+
+
+# ---------------------------------------------------------------------
+with st.container(border=True):
+    st.html('<h5>If you work with target CPA</h5>')
+
+    col_equation, col_non_equation = st.columns([1, 10])
+    with col_equation:
+        st.latex(r'''
+                \begin{equation*}
+                \begin{aligned}
+                \text{CPA} = \frac{\text{rev per acquisition}}{\text{target ROAS}}
+                \end{aligned}
+                \end{equation*}
+                ''')
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        with st.container(border=True, height=100):
+            rev_per_conv = st.number_input('Revenue per acquisition (or conversion)',
+                                           min_value=0.0,
+                                           step=0.01,
+                                           value=40.25,
+                                           placeholder="Type a number..."
+                                           )
+
+    with col2:
+        with st.container(border=True, height=100):
+            tROAS_cpa = st.number_input('Target ROAS (if you type 400, this means 400%) ',
+                                        min_value=0,
+                                        step=1,
+                                        value=400,
+                                        placeholder="Type a number..."
+                                        )
+
+    with col3:
+        with st.container(border=True, height=100):
+            if rev_per_conv is not None and tROAS_cpa is not None:
+                calculated_CPA = np.round(rev_per_conv / (tROAS_cpa/100), 3)
+
+                st.write('tCPA:')
+                st.write(calculated_CPA)
+
+
+            else:
+                st.warning('Please provide a value for revenue per acquisition and target ROAS')
+
+# ---------------------------------------------------------------------
+with st.container(border=True):
+    st.html('<h5>If you want to determine what your ROAS should be based on profit margins</h5>')
+
+    col_equation, col_non_equation = st.columns([1, 10])
+    with col_equation:
+        st.latex(r'''
+                    \begin{equation*}
+                    \begin{aligned}
+                    \text{Break even ROAS} = \frac{1}{\text{Average profit margin}}
+                    \end{aligned}
+                    \end{equation*}
+                    ''')
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        with st.container(border=True, height=100):
+            avg_profit_margin = st.number_input('Average profit margin (if you type 400, this means 400%)',
+                                                min_value=0.0,
+                                                step=0.01,
+                                                value=40.0,
+                                                placeholder="Type a number..."
+                                                )
+
+    with col2:
+        with st.container(border=True, height=100):
+            if avg_profit_margin is not None:
+                calculated_tROAS = np.round(1 / (avg_profit_margin/100), 3)
+
+                st.write('tROAS:')
+                st.write(calculated_tROAS)
+
+
+            else:
+                st.warning('Please provide a value for average profit margin')
+
+
+
+
